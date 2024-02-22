e06cab9222fd43102dfe747cabfce852216852d9
LucaBassi <luca.bassi@cpnv.ch>
2024-02-22 21:10:33 +0100

    Update install.sh

--- Kebernetes-Cluster/install.sh
+++ Kebernetes-Cluster/install.sh
@@ -4,17 +4,13 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list
-
 sudo apt update
 
 sudo apt install kubeadm kubelet kubectl
 sudo apt-mark hold kubeadm kubelet kubectl
 
-
 sudo swapoff -a
-
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 sudo tee /etc/modules-load.d/containerd.conf <<EOF
@@ -53,12 +49,13 @@ sudo tee /etc/docker/daemon.json <<EOF
 EOF
 
 sudo systemctl daemon-reload && sudo systemctl restart docker
-sudo nano /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 sudo sed -i  '/\[Service\]/a Environment=\"KUBELET_EXTRA_ARGS=--fail-swap-on=false\"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 sudo systemctl daemon-reload && sudo systemctl restart kubelet
+
 sudo kubeadm init --control-plane-endpoint=mas-masternode-01 --upload-certs
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
